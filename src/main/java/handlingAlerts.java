191613d6c5cb6b8ef6c800d108b338c3d309cf2b
Kaustubh <kaustubhmthakare@gmail.com>
2023-08-20 13:49:14 +0530

    Added handlingAlerts file.

--- src/main/java/handlingAlerts.java
+++ src/main/java/handlingAlerts.java
@@ -1,2 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
 public class handlingAlerts {
+    public static void main(String[] args) throws InterruptedException {
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.msedge.driver","D:\\edgedriver_win32\\msedgedriver");
+        WebDriver driver = new EdgeDriver(options);
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("name")).sendKeys("Tester");
+        driver.findElement(By.id("alertbtn")).click();
+        Thread.sleep(1000);
+        driver.switchTo().alert().accept();
+        driver.findElement(By.id("name")).sendKeys("Tester");
+        driver.findElement(By.id("confirmbtn")).click();
+        Thread.sleep(1000);
+        driver.switchTo().alert().dismiss();
+        driver.quit();
+    }
 }
