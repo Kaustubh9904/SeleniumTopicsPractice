b255f4b8319cca41fc536012e548546a1cf68f23
Kaustubh <kaustubhmthakare@gmail.com>
2023-08-24 23:06:46 +0530

    Ecom add to cart for specific item test script added.

--- src/main/java/Ecomm_App_Add_to_cart.java
+++ src/main/java/Ecomm_App_Add_to_cart.java
@@ -11,21 +11,21 @@ public class Ecomm_App_Add_to_cart {
     public static void main(String[] args) {
         EdgeOptions options = new EdgeOptions();
         options.addArguments("--remote-allow-origins=*");
-        System.setProperty("webdriver.msedge.driver","D:\\edgedriver_win32\\msedgedriver");
+        System.setProperty("webdriver.msedge.driver", "D:\\edgedriver_win32\\msedgedriver");
         WebDriver driver = new EdgeDriver(options);
         driver.get("https://rahulshettyacademy.com/seleniumPractise/");
         driver.manage().window().maximize();
-        List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-
-        for(int i=0;i<products.size();i++){
-           String name = products.get(i).getText();
-
-           if(name.contains("Cucumber")){
-            driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-            break;
-           }
+        //get list of webelements by the used xpath in a variable called products
+        List<WebElement> products = driver.findElements(By.xpath("//h4[@class='product-name']"));
+        //loop through the list of elements in products, store text of that element on that particulr index using
+        //productName string using get(i) method. Then if it matches requirement click on current index element.
+        for (int i = 0; i < products.size(); i++) {
+            String productName = products.get(i).getText();
+            if (productName.contains("Beetroot")) {
+                driver.findElements(By.cssSelector("a.increment")).get(i).click();
+                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+            }
         }
-
         driver.quit();
     }
 }
